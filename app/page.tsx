2eb5e284ae95c22209635155a08281ea9c9c4f9c
mohithingorani <mohithingorani2003@gmail.com>
2026-01-20 02:23:49 +0530

    added custom scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="w-3xl relative p-8 border-[0.2px] border-white/10 rounded-3xl bg-[#1e1e1f]">
+        <div className="w-3xl overflow-x-hidden relative p-8 border-[0.2px] border-white/10 rounded-3xl bg-[#1e1e1f]">
           <div className="absolute text-sm flex gap-8 top-0 right-0 border-[0.2px] border-white/10 bg-[#282829] rounded-tr-3xl *:hover:text-yellow-400 *:cursor-pointer rounded-bl-3xl px-8 py-4 ">
             {buttons.map((buttonval, index) => {
               return (
