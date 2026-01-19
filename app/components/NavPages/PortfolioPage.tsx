434460bc78c0f28be19bc4e66b131261a50fe5d7
mohithingorani <mohithingorani2003@gmail.com>
2026-01-20 02:57:57 +0530

    make project component functional

--- app/components/NavPages/PortfolioPage.tsx
+++ app/components/NavPages/PortfolioPage.tsx
@@ -1,8 +1,5 @@
-import { doingData } from "@/app/data";
+import ProjectComponent from "../ProjectComponent";
 import UnderLine from "../Underline";
-import { DoingBox } from "../DoingBox";
-import Image from "next/image";
-import { EyeIcon } from "../Icons";
 
 export default function PortfolioPage() {
   return (
@@ -11,40 +8,11 @@ export default function PortfolioPage() {
       <div className="my-4 ">
         <UnderLine />
       </div>
-      <div className="mt-8">
-        <Project />
+      <div className="mt-8 grid grid-cols-2">
+        <ProjectComponent image="voltex.JPG" heading="Self Hosted Paas" description="Devops" link="" />
+        <ProjectComponent image="social.JPG" heading="Social Network" description="Full Stack" link="" />
       </div>
     </div>
   );
 }
 
-function Project() {
-  return (
-    <div>
-      <div className="group cursor-pointer relative w-80 h-40 overflow-hidden rounded-xl">
-        {/* Image */}
-        <Image
-          src="/voltex.jpg"
-          alt=""
-          fill
-          className="object-cover transition-transform duration-300 group-hover:scale-110"
-        />
-
-        {/* Overlay */}
-        <div
-          className="absolute inset-0 flex items-center justify-center
-        bg-black/50 opacity-0 transition-opacity duration-300
-        group-hover:opacity-100"
-        >
-          <span className=" w-15 h-15 flex justify-center items-center rounded-md bg-[#1e1e1f] p-2">
-            <EyeIcon className="opacity-80" />
-          </span>
-        </div>
-      </div>
-      <div className="ml-2 mt-2 poppins">
-        <div className="text-white/90">Self Deployed PaaS</div>
-        <div className="text-white/60">Devops</div>
-      </div>
-    </div>
-  );
-}
---
+++ app/components/ProjectComponent.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { EyeIcon } from "./Icons";
+
+export default function ProjectComponent({
+  image,
+  link,
+  heading,
+  description
+}:{
+  image:string,
+  link:string,
+  heading:string,
+  description:string
+}) {
+  return (
+    <div className="mb-6">
+      <div className="group cursor-pointer relative w-80 h-40 overflow-hidden rounded-xl">
+        {/* Image */}
+        <Image
+          src={`/projects/${image}`}
+          alt=""
+          fill
+          className="object-cover transition-transform duration-300 group-hover:scale-110"
+        />
+
+        {/* Overlay */}
+        <div
+          className="absolute inset-0 flex items-center justify-center
+        bg-black/50 opacity-0 transition-opacity duration-300
+        group-hover:opacity-100"
+        >
+          <span className=" w-15 h-15 flex justify-center items-center rounded-md bg-[#1e1e1f] p-2">
+            <EyeIcon className="opacity-80" />
+          </span>
+        </div>
+      </div>
+      <div className="ml-2 mt-2 poppins">
+        <div className="text-white/90">{heading}</div>
+        <div className="text-white/60">{description}</div>
+      </div>
+    </div>
+  );
+}
