41ef741354472d00f627107ca27b95ded606d17f
mohithingorani <mohithingorani2003@gmail.com>
2026-01-20 18:15:18 +0530

    resume page

--- app/components/SkillSection.tsx
+++ app/components/SkillSection.tsx
@@ -8,13 +8,13 @@ export default function Skills() {
 
   const currentDomain = Domains.find((d) => d.name === selectedDomain)!;
   return (
-    <div className="w-full  border rounded-2xl border-white/10 p-8">
+    <div className="w-full  border rounded-2xl border-white/10 p-3 md:p-6 lg:p-8">
       <div className="flex gap-3 mb-6">
         {ALL_DOMAINS.map((domain) => (
           <button
             key={domain}
             onClick={() => setSelectedDomain(domain)}
-            className={`border rounded-md px-3 py-1 text-md transition
+            className={`border rounded-md  px-2 py-1 md:px-3 md:py-1 text-xs  sm:text-md transition
               ${
                 selectedDomain === domain
                   ? "text-yellow-400/80 font-semibold bg-yellow-300/10 border-yellow-300/30"
@@ -25,7 +25,7 @@ export default function Skills() {
           </button>
         ))}
       </div>
-      <div className="w-full h-90 ">
+      <div className="w-full h-60 sm:h-70 md:h-90 ">
         <SkillRadar data={currentDomain.data} labels={currentDomain.labels} />
       </div>
     </div>
