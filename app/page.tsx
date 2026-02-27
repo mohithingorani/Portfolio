fd2f239db5b0e286b755ce79017763db109a5987
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-02-28 03:46:50 +0530

    small

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import RightSection from "./components/RightSection";
 export default function Home() {
   return (
     <div className="flex justify-center lg:px-3 lg:py-6 2xl:py-12 text-white w-full poppins-normal">
-      <div className="flex w-full  flex-col xl:flex-row gap-3 lg:gap-12  xl:gap-8 p-6 md:py-8 lg:px-18 lg:justify-center   ">
+      <div className="flex w-full  flex-col xl:flex-row gap-3 lg:gap-12  xl:gap-8 p-3 md:py-8 lg:px-18 lg:justify-center   ">
         
         {/* Section Left */}
         <div className="  ">
