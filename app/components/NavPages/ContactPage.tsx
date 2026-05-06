f2f60572a06c4c7bcc63c7c08402e238c34fbb43
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-05-06 21:45:19 +0530

    hyperlink added

--- app/components/NavPages/ContactPage.tsx
+++ app/components/NavPages/ContactPage.tsx
@@ -1,17 +1,24 @@
 import ContactForm from "../ContactForm";
 import UnderLine from "../Underline";
+
 export default function ContactPage() {
   return (
     <div>
-      <div className="poppins-semibold h-full text-3xl poppins-normal">Contact</div>
+      <div className="poppins-semibold h-full text-3xl poppins-normal">
+        Contact
+      </div>
       <div className="my-4 ">
         <UnderLine />
       </div>
-      <div className=" mt-6">
-       <ContactForm/>
+      <div className="mt-6 space-y-6">
+        <a
+          href="mailto:mohithingorani20003@gmail.com"
+          className="text-yellow-200 hover:text-yellow-100 hover:underline transition-all"
+        >
+          mohithingorani20003@gmail.com
+        </a>
+        <ContactForm />
       </div>
     </div>
   );
 }
-
-
